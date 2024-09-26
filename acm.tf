c0c81153ccdb8ca19b79b029f4e729a2cfe5248b
l20103234 <l20103234@gmail.com>
2024-09-26 10:17:11 +0900

    Update acm.tf

--- acm.tf
+++ acm.tf
@@ -11,19 +11,3 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
-
-# 인증서 검증
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-}
